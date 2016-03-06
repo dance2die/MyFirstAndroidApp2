20d18ab9910dc58aba139cc98f102a689c5aefc7
dance2die <dontbothermewithspam@gmail.com>
2016-03-06 09:23:02 -0500

    Added a button and an event listener that handles the button click.

--- app/src/main/java/com/dance2die/myfirstandroidapp2/MainActivity.java
+++ app/src/main/java/com/dance2die/myfirstandroidapp2/MainActivity.java
@@ -5,11 +5,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+        Log.i("Info", "Button clicked!");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
